8e665650c3a828560da47d7d5750f749898381e9
Jun <nbc7799@naver.com>
2025-11-01 00:20:19 +0900

    ♻️ Refactor: localfont 절대경로 원복 #16

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 };
 
 const pretendard = localFont({
-  src: '@/app/fonts/PretendardVariable.woff2',
+  src: './fonts/PretendardVariable.woff2',
   variable: '--font-pretendard',
   display: 'swap',
   weight: '100 900',

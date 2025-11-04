e5c6039e3037f56c650479b90db99b28cf3e220d
minji_105 <ajhjmj9@naver.com>
2025-11-05 00:38:16 +0900

    ✨ Feature: 미디어쿼리 브레이크포인트 추가 (#33)

--- src/constants/breakpoint.ts
+++ src/constants/breakpoint.ts
@@ -1,5 +1,7 @@
-export const BREAKPOINT_MOBILE = 640;
+export const BREAKPOINT_MOBILE = 480;
+export const BREAKPOINT_TABLET = 768;
 
 export const MEDIA_QUERY = {
   mobile: `(max-width: ${BREAKPOINT_MOBILE}px)`,
+  tablet: `(max-width: ${BREAKPOINT_TABLET}px)`,
 };

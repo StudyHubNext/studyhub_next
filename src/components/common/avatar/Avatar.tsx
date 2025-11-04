9b799081823b6a8016191cee695b580c3c2010c1
minji_105 <ajhjmj9@naver.com>
2025-11-04 17:29:45 +0900

    ✨ Feature: 아바타 컴포넌트 추가 (#24)

---
+++ src/components/common/avatar/Avatar.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/utils';
+import { type VariantProps } from 'class-variance-authority';
+
+import Image from 'next/image';
+import { avatarVariants } from './avatarVariants';
+
+interface AvatarProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof avatarVariants> {
+  name: string;
+  profileImage?: string;
+  className?: string;
+}
+
+export default function Avatar({ size, name, profileImage, className }: AvatarProps) {
+  return (
+    <div className={cn(avatarVariants({ size }), className)}>
+      {profileImage ? (
+        <Image className='h-full w-full object-cover' src={profileImage} alt={name} />
+      ) : (
+        <>{name.charAt(0)}</>
+      )}
+    </div>
+  );
+}

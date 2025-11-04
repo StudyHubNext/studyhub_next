6c274e23eb04f566921eb4e3038fe7b64cf75de7
minji_105 <ajhjmj9@naver.com>
2025-11-04 19:22:39 +0900

    ♻️ Refactor: 아바타 컴포넌트 코드 개선 (#24)
    - props 타입 일치
    - 나머지 props 전파
    - next/Image 최적화
    - fragment 제거

--- src/components/common/avatar/Avatar.tsx
+++ src/components/common/avatar/Avatar.tsx
@@ -5,26 +5,36 @@ import Image from 'next/image';
 import { avatarVariants } from './avatarVariants';
 
 interface AvatarProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof avatarVariants> {
   name: string;
   profileImage?: string;
   className?: string;
 }
 
-export default function Avatar({ size, name, profileImage, className }: AvatarProps) {
+const SIZE_IN_PIXELS = {
+  xs: 24,
+  sm: 32,
+  md: 40,
+  lg: 48,
+  xl: 64,
+};
+
+export default function Avatar({ size, name, profileImage, className, ...props }: AvatarProps) {
+  const imageSize = size ? SIZE_IN_PIXELS[size] : SIZE_IN_PIXELS.md;
+
   return (
-    <div className={cn(avatarVariants({ size }), className)}>
+    <div {...props} className={cn(avatarVariants({ size }), className)}>
       {profileImage ? (
         <Image
-          width={100}
-          height={100}
+          width={imageSize}
+          height={imageSize}
           className='h-full w-full object-cover'
           src={profileImage}
           alt={name}
         />
       ) : (
-        <>{name.charAt(0)}</>
+        name.charAt(0)
       )}
     </div>
   );

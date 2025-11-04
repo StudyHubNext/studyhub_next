522ea79619da13e492462dc0e747083ffd78a169
minji_105 <ajhjmj9@naver.com>
2025-11-04 22:24:22 +0900

    ✨ Feature: 배지 공통 컴포넌트 추가 (#27)

---
+++ src/components/common/badge/Badge.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/utils';
+import { badgeVariants } from './badgeVariants';
+
+interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {
+  content: string;
+  className?: string;
+}
+
+export default function Badge({ variant, size, className, content, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant, size }), className)} {...props}>
+      {content}
+    </div>
+  );
+}

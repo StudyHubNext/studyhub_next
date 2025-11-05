316b08cde1e5c97a76caafebd30213842d965d8d
minji_105 <ajhjmj9@naver.com>
2025-11-05 13:03:41 +0900

    ✨ Feature: 로고 컴포넌트 추가 (#36)

---
+++ src/components/common/logo/Logo.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/utils';
+import { type VariantProps } from 'class-variance-authority';
+
+import Text from '@/components/common/text/Text';
+import Link from 'next/link';
+import { logoVariants } from './logoVariants';
+
+interface LogoProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof logoVariants> {}
+
+export default function Logo({ size, className, ...props }: LogoProps) {
+  return (
+    <Link href='/' className='flex items-center gap-2'>
+      <div {...props} className={cn(logoVariants({ size }), className)}>
+        S
+      </div>
+      {size !== 'sm' && <Text className='text-primary-600 text-xl font-bold'>StudyHub</Text>}
+    </Link>
+  );
+}

06781a4284c16b8d3df21b8d6815631ec9add251
minji_105 <ajhjmj9@naver.com>
2025-11-05 13:32:55 +0900

    🔧 Chore: vitest 설정에 @vitejs/plugin-react import 추가 (#36)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,14 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 const dirname =
   typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',

199fce4d645e456bc0007336edd6f2f165bf5859
minji_105 <ajhjmj9@naver.com>
2025-11-05 13:04:12 +0900

    ♻️ Refactor: commoon/ 배럴 파일 업데이트 (#36)

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,5 @@
 export * from '@/components/common/avatar';
 export * from '@/components/common/badge';
 export * from '@/components/common/button';
+export * from '@/components/common/logo';
 export * from '@/components/common/text';

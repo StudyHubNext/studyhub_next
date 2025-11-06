dc5a3a5f68ee69bf36ddf45b1b75b4e069249aac
minji_105 <ajhjmj9@naver.com>
2025-11-07 02:49:56 +0900

    ✨ Feature: UserDropdown 컴포넌트 상태 관리 로직 리팩토링 (#33)

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,9 +1,8 @@
 import { useMediaQuery } from 'react-responsive';
 
 import { MEDIA_QUERY } from '@/constants';
-import { Avatar, Button, Logo, Text } from '@/components/common';
-import { useState } from 'react';
-import { Bars3Icon, ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
+import { Avatar, Logo, Text } from '@/components/common';
+import { Bars3Icon } from '@heroicons/react/20/solid';
 import AuthButtonGroup from '../AuthButtonGroup';
 import UserDropdown from './UserDropdown';
 import HeaderNav from './HeaderNav';
@@ -14,11 +13,6 @@ interface HeaderProps {
 
 export default function Header({ isLoggedIn }: HeaderProps) {
   const isMobile = useMediaQuery({ query: MEDIA_QUERY.mobile });
-  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
-
-  const handleToggleDropdown = () => {
-    setIsDropdownOpen((prev) => !prev);
-  };
 
   return (
     <header className='fixed z-50 h-16 w-full border-b border-gray-200 bg-white px-4 md:px-20'>
@@ -40,18 +34,7 @@ export default function Header({ isLoggedIn }: HeaderProps) {
               <div className='flex items-center gap-2'>
                 <Avatar profileImage='/profile-test.webp' name={'김개발'} size='sm' />
                 <Text className='text-primary-600'>{'김개발'}</Text>
-                {!isMobile && (
-                  <div className='relative'>
-                    <Button
-                      variant='ghost'
-                      className='w-8 p-2'
-                      onClick={() => handleToggleDropdown()}
-                    >
-                      {isDropdownOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-                    </Button>
-                    <UserDropdown isOpen={isDropdownOpen} />
-                  </div>
-                )}
+                <UserDropdown />
               </div>
             ) : (
               <>{!isMobile && <AuthButtonGroup />}</>
--- src/components/layout/header/UserDropdown.tsx
+++ src/components/layout/header/UserDropdown.tsx
@@ -1,31 +1,48 @@
+import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { ArrowLeftOnRectangleIcon, UserIcon } from '@heroicons/react/24/outline';
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
 
 import { Button } from '@/components/common';
+import { MEDIA_QUERY } from '@/constants';
 
-interface UserDropdownProps {
-  isOpen: boolean;
-}
+export default function UserDropdown() {
+  const isMobile = useMediaQuery({ query: MEDIA_QUERY.mobile });
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
-export default function UserDropdown({ isOpen }: UserDropdownProps) {
-  if (!isOpen) return null;
+  const handleToggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
 
   return (
-    <div className='absolute top-10 right-2 flex w-48 flex-col rounded-lg border border-gray-200 bg-white py-2 shadow-md'>
-      <div className='px-2'>
-        <Button variant='ghost' className='w-full gap-3 p-2'>
-          <UserIcon width={14} />
-          마이페이지
-        </Button>
-      </div>
-      <div className='py-2'>
-        <div className='border border-gray-100' />
-      </div>
-      <div className='px-2'>
-        <Button variant='ghost' className='text-danger-600 w-full gap-3 p-2'>
-          <ArrowLeftOnRectangleIcon width={14} className='text-danger-600' />
-          로그아웃
-        </Button>
-      </div>
-    </div>
+    <>
+      {!isMobile && (
+        <div className='relative'>
+          <Button variant='ghost' className='w-8 p-2' onClick={() => handleToggleDropdown()}>
+            {isDropdownOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          </Button>
+
+          {isDropdownOpen && (
+            <div className='absolute top-10 right-2 flex w-48 flex-col rounded-lg border border-gray-200 bg-white py-2 shadow-md'>
+              <div className='px-2'>
+                <Button variant='ghost' className='w-full gap-3 p-2'>
+                  <UserIcon width={14} />
+                  마이페이지
+                </Button>
+              </div>
+              <div className='py-2'>
+                <div className='border border-gray-100' />
+              </div>
+              <div className='px-2'>
+                <Button variant='ghost' className='text-danger-600 w-full gap-3 p-2'>
+                  <ArrowLeftOnRectangleIcon width={14} className='text-danger-600' />
+                  로그아웃
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </>
   );
 }

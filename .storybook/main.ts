c14eb1a41f778fd3740298dc19b53e4f7fc920f9
minji_105 <ajhjmj9@naver.com>
2025-11-07 01:57:52 +0900

    🔧 Chore: 스토리북에 viteFinal 설정 추가 (#33)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,36 @@
-import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
   ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/nextjs-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/nextjs-vite',
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  staticDirs: ['../public'],
+  viteFinal: async (config) => {
+    config.optimizeDeps = {
+      ...(config.optimizeDeps || {}),
+      include: [
+        ...(config.optimizeDeps?.include || []),
+        '@heroicons/react/24/outline',
+        '@heroicons/react/24/solid',
+      ],
+      exclude: ['@storybook/nextjs', '@storybook/nextjs-vite'],
+    };
+
+    config.ssr = {
+      ...(config.ssr || {}),
+      noExternal: ['@heroicons/react'],
+    };
+
+    return config;
+  },
 };
+
 export default config;

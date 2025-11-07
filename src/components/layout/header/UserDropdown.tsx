21b3254cfa7effcfb9ce941d52c3abfdb6ed7a57
minji_105 <ajhjmj9@naver.com>
2025-11-07 12:00:29 +0900

    ♻️ Refactor: tailwindcss 사용하여 반응형 분기 (#33)

--- src/components/layout/header/UserDropdown.tsx
+++ src/components/layout/header/UserDropdown.tsx
@@ -1,13 +1,10 @@
 import { useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
 import { ArrowLeftOnRectangleIcon, UserIcon } from '@heroicons/react/24/outline';
 import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/20/solid';
 
 import { Button } from '@/components/common';
-import { MEDIA_QUERY } from '@/constants';
 
 export default function UserDropdown() {
-  const isMobile = useMediaQuery({ query: MEDIA_QUERY.mobile });
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
 
   const handleToggleDropdown = () => {
@@ -15,34 +12,30 @@ export default function UserDropdown() {
   };
 
   return (
-    <>
-      {!isMobile && (
-        <div className='relative'>
-          <Button variant='ghost' className='w-8 p-2' onClick={() => handleToggleDropdown()}>
-            {isDropdownOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-          </Button>
+    <div className='xs:block relative hidden'>
+      <Button variant='ghost' className='w-8 p-2' onClick={() => handleToggleDropdown()}>
+        {isDropdownOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+      </Button>
 
-          {isDropdownOpen && (
-            <div className='absolute top-10 right-2 flex w-48 flex-col rounded-lg border border-gray-200 bg-white py-2 shadow-md'>
-              <div className='px-2'>
-                <Button variant='ghost' className='w-full gap-3 p-2'>
-                  <UserIcon width={14} />
-                  마이페이지
-                </Button>
-              </div>
-              <div className='py-2'>
-                <div className='border border-gray-100' />
-              </div>
-              <div className='px-2'>
-                <Button variant='ghost' className='text-danger-600 w-full gap-3 p-2'>
-                  <ArrowLeftOnRectangleIcon width={14} className='text-danger-600' />
-                  로그아웃
-                </Button>
-              </div>
-            </div>
-          )}
+      {isDropdownOpen && (
+        <div className='absolute top-10 right-2 flex w-48 flex-col rounded-lg border border-gray-200 bg-white py-2 shadow-md'>
+          <div className='px-2'>
+            <Button variant='ghost' className='w-full gap-3 p-2'>
+              <UserIcon width={14} />
+              마이페이지
+            </Button>
+          </div>
+          <div className='py-2'>
+            <div className='border border-gray-100' />
+          </div>
+          <div className='px-2'>
+            <Button variant='ghost' className='text-danger-600 w-full gap-3 p-2'>
+              <ArrowLeftOnRectangleIcon width={14} className='text-danger-600' />
+              로그아웃
+            </Button>
+          </div>
         </div>
       )}
-    </>
+    </div>
   );
 }

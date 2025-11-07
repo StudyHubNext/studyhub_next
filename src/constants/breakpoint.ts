9491b13db63368e927879efb0b3a8b9c4b3adff3
minji_105 <ajhjmj9@naver.com>
2025-11-07 11:55:52 +0900

    🔥 Delete: breakpoint 상수 파일 삭제 (#33)

--- src/constants/breakpoint.ts
+++
@@ -1,7 +0,0 @@
-export const BREAKPOINT_MOBILE = 480;
-export const BREAKPOINT_TABLET = 768;
-
-export const MEDIA_QUERY = {
-  mobile: `(max-width: ${BREAKPOINT_MOBILE}px)`,
-  tablet: `(max-width: ${BREAKPOINT_TABLET}px)`,
-};

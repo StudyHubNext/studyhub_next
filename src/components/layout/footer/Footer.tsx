193e482975cf167e36e384a60a4bf8059834b1b1
minji_105 <ajhjmj9@naver.com>
2025-11-10 20:07:22 +0900

    ♻️ Refactor: Footer 스타일 및 구조 개선 (#34)

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { H, Text } from '@/components/common';
+import { Logo, Text } from '@/components/common';
 import Link from 'next/link';
 
 export const FOOTER_NAV_LISTS = [
@@ -23,35 +23,31 @@ export const FOOTER_NAV_LISTS = [
 export default function Footer() {
   return (
     <footer className='flex justify-center bg-gray-900 px-4 py-8 md:px-20 md:py-12'>
-      <div className='w-full max-w-7xl'>
+      <div className='flex w-full max-w-7xl flex-col gap-8'>
         <div className='grid gap-8 md:grid-cols-4'>
-          <div className='col-span-2'>
-            <Link href='/'>
-              <H level={3} className='text-primary-400 mb-4'>
-                StudyHub
-              </H>
-            </Link>
+          <div className='col-span-2 flex flex-col gap-4'>
+            <Logo />
             <Text className='text-gray-300'>
               IT 전문가로 성장하는 여정에 함께합니다. 최고의 강의와 스터디 그룹으로 실무 역량을
               키워보세요.
             </Text>
           </div>
           {FOOTER_NAV_LISTS.map((navItem) => (
-            <nav key={navItem.title}>
-              <H level={4} className='mb-4 text-base font-semibold text-white'>
-                {navItem.title}
-              </H>
+            <nav key={navItem.title} className='flex flex-col gap-4'>
+              <Text className='font-semibold text-white'>{navItem.title}</Text>
               <ul className='flex flex-col gap-2 text-gray-300'>
                 {navItem.list.map((el) => (
                   <li key={el.name}>
-                    <Link href={el.path}>{el.name}</Link>
+                    <Link href={el.path} className='hover:underline'>
+                      {el.name}
+                    </Link>
                   </li>
                 ))}
               </ul>
             </nav>
           ))}
         </div>
-        <div className='mt-8 flex justify-center pt-8'>
+        <div className='xs:pt-8 flex justify-center pt-4'>
           <Text className='text-gray-400'>© 2024 StudyHub. All rights reserved</Text>
         </div>
       </div>

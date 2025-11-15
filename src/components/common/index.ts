a12ca3257b034db74923fe35c21ef58488979d86
minji_105 <ajhjmj9@naver.com>
2025-11-15 18:52:24 +0900

    ✨ Feature: input 공통 컴포넌트 추가 (#44)

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,6 +3,7 @@ export * from '@/components/common/badge';
 export * from '@/components/common/button';
 export * from '@/components/common/card';
 export * from '@/components/common/image-card';
+export * from '@/components/common/input';
 export * from '@/components/common/logo';
 export * from '@/components/common/markdown-editor';
 export * from '@/components/common/text';

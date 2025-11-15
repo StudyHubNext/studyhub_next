ac0f995a7bc4fa62f0f31a4a057e1a60f9565392
minji_105 <ajhjmj9@naver.com>
2025-11-15 18:52:24 +0900

    ✨ Feature: input 공통 컴포넌트 추가 (#44)

---
+++ src/components/common/input/Input.tsx
@@ -0,0 +1,41 @@
+import { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
+import { cn } from '@/utils';
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+  errorMessage?: string;
+  icon?: ReactNode;
+}
+
+function Input(
+  { className, type, error = false, errorMessage, icon, ...props }: InputProps,
+  ref: React.Ref<HTMLInputElement>,
+) {
+  return (
+    <div className='w-full'>
+      <div className='relative flex items-center'>
+        {icon && (
+          <div className='pointer-events-none absolute left-3 flex items-center'>{icon}</div>
+        )}
+        <input
+          type={type}
+          className={cn(
+            'flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400',
+            'focus:ring-primary-500/50 focus:border-primary-500 focus:ring-2 focus:outline-none',
+            'disabled:cursor-not-allowed disabled:opacity-50',
+            {
+              'border-danger-500 focus:ring-danger-500/50': error,
+              'pl-10': icon,
+            },
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
+      {error && errorMessage && <p className='text-danger-500 mt-1 text-xs'>{errorMessage}</p>}
+    </div>
+  );
+}
+
+export default forwardRef<HTMLInputElement, InputProps>(Input);

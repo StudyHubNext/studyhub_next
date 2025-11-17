0c241cfe97d7e48fef505c6664ee3c172e140533
minji_105 <ajhjmj9@naver.com>
2025-11-17 23:01:05 +0900

    ♻️ Refactor: 경로 중복 문자열 상수화 (#52)

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,19 +1,26 @@
+const GROUPS_BASE = '/groups';
+const GROUP_DETAIL_BASE = (groupId: string | number) => `${GROUPS_BASE}/${groupId}`;
+const GROUP_RECORDS_BASE = (groupId: string | number) => `${GROUP_DETAIL_BASE(groupId)}/records`;
+const GROUP_RECORD_DETAIL_BASE = (groupId: string | number, recordId: string | number) =>
+  `${GROUP_RECORDS_BASE(groupId)}/${recordId}`;
+
 export const PATHS = {
   HOME: '/',
   LOGIN: '/login',
   SIGNUP: '/signup',
   MYPAGE: '/mypage',
+
   GROUPS: {
-    LIST: '/groups',
-    CREATE: '/groups/create',
-    DETAIL: (groupId: string | number) => `/groups/${groupId}`,
-    EDIT: (groupId: string | number) => `/groups/${groupId}/edit`,
+    LIST: GROUPS_BASE,
+    CREATE: `${GROUPS_BASE}/create`,
+    DETAIL: GROUP_DETAIL_BASE,
+    EDIT: (groupId: string | number) => `${GROUP_DETAIL_BASE(groupId)}/edit`,
+
     RECORDS: {
-      CREATE: (groupId: string | number) => `/groups/${groupId}/records/create`,
-      DETAIL: (groupId: string | number, recordId: string | number) =>
-        `/groups/${groupId}/records/${recordId}`,
+      CREATE: (groupId: string | number) => `${GROUP_RECORDS_BASE(groupId)}/create`,
+      DETAIL: GROUP_RECORD_DETAIL_BASE,
       EDIT: (groupId: string | number, recordId: string | number) =>
-        `/groups/${groupId}/records/${recordId}/edit`,
+        `${GROUP_RECORD_DETAIL_BASE(groupId, recordId)}/edit`,
     },
   },
 } as const;

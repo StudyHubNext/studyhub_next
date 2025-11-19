7cd73d8300444952e3a59addc61bc84e3ff6f74a
minji_105 <ajhjmj9@naver.com>
2025-11-19 15:53:17 +0900

    ♻️ Refactor: 변수명 camelCase로 통일 (#54)

--- src/mocks/lectures.mock.ts
+++ src/mocks/lectures.mock.ts
@@ -4,7 +4,7 @@ export const LECTURES = [
     title: 'React 완벽 마스터 강의',
     instructor: '김개발',
     price: 89000,
-    discounted_price: 59000,
+    discountedPrice: 59000,
     image: '/lecture/react.png',
   },
   {
@@ -12,7 +12,7 @@ export const LECTURES = [
     title: 'Python 데이터 사이언스',
     instructor: '이데이터',
     price: 150000,
-    discounted_price: 99000,
+    discountedPrice: 99000,
     image: '/lecture/data.png',
   },
   {
@@ -20,7 +20,7 @@ export const LECTURES = [
     title: 'AWS 클라우드 아키텍처',
     instructor: '한클라우드',
     price: 180000,
-    discounted_price: 129000,
+    discountedPrice: 129000,
     image: '/lecture/aws.png',
   },
 ];

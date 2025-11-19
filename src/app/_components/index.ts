ddc0d5c74efa2f17764b33c73c118debe68f0f4b
minji_105 <ajhjmj9@naver.com>
2025-11-19 15:20:09 +0900

    ✨ Feature: app/_components에 배럴 파일 추가 (#54)

---
+++ src/app/_components/index.ts
@@ -0,0 +1,4 @@
+export { default as HeroSection } from '@/app/_components/HeroSection';
+export { default as FeatureSection } from '@/app/_components/FeatureSection';
+export { default as LectureSection } from '@/app/_components/LectureSection';
+export { default as CallToActionSection } from '@/app/_components/CallToActionSection';

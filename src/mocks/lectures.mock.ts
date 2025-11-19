78b86cef34424c43140831d7d1415aaa7268575a
minji_105 <ajhjmj9@naver.com>
2025-11-19 14:52:33 +0900

    ✨ Feature: 강의 목록 목 데이터 추가 (#54)

---
+++ src/mocks/lectures.mock.ts
@@ -0,0 +1,26 @@
+export const LECTURES = [
+  {
+    id: 1,
+    title: 'React 완벽 마스터 강의',
+    instructor: '김개발',
+    price: 89000,
+    discounted_price: 59000,
+    image: '/lecture/react.png',
+  },
+  {
+    id: 2,
+    title: 'Python 데이터 사이언스',
+    instructor: '이데이터',
+    price: 150000,
+    discounted_price: 99000,
+    image: '/lecture/data.png',
+  },
+  {
+    id: 3,
+    title: 'AWS 클라우드 아키텍처',
+    instructor: '한클라우드',
+    price: 180000,
+    discounted_price: 129000,
+    image: '/lecture/aws.png',
+  },
+];

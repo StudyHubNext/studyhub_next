5b88d4b9017a73d0047a9567fa52372fd55fc06d
minji_105 <ajhjmj9@naver.com>
2025-11-25 00:42:39 +0900

    ♻️ Refactor: 사이드바 푸터 컴포넌트 분리 (#35)

--- src/components/layout/side-bar/SideBar.tsx
+++ src/components/layout/side-bar/SideBar.tsx
@@ -1,7 +1,5 @@
-import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
-import { Avatar, Button, Text } from '@/components/common';
 import { cn } from '@/utils';
-import { SideBarHeader, SideBarNav } from '.';
+import { SideBarFooter, SideBarHeader, SideBarNav } from '.';
 
 interface HeaderSideMenuProps {
   isLoggedIn: boolean;
@@ -26,25 +24,7 @@ export default function SideMenu({ isLoggedIn, isOpen, onClose }: HeaderSideMenu
           <SideBarNav />
         </div>
 
-        {isLoggedIn ?? (
-          <footer className='border-t border-gray-200 p-4'>
-            <div className='flex flex-col gap-4'>
-              <div className='flex items-center gap-3'>
-                <Avatar profileImage='/profile-test.webp' name='김개발' size='md' />
-                <div className='flex flex-col'>
-                  <Text className='font-semibold'>김 개발</Text>
-                  <Text variant='extraSmall' className='text-gray-600'>
-                    kim.dev@example.com
-                  </Text>
-                </div>
-              </div>
-              <Button variant='secondary' size='lg' className='justify-center px-4 py-2'>
-                <ArrowLeftOnRectangleIcon width={18} className='h-7' />
-                로그아웃
-              </Button>
-            </div>
-          </footer>
-        )}
+        {isLoggedIn ?? <SideBarFooter />}
       </div>
     </div>
   );
--- src/components/layout/side-bar/index.ts
+++ src/components/layout/side-bar/index.ts
@@ -1,2 +1,3 @@
 export { default as SideBarHeader } from '@/components/layout/side-bar/SideBarHeader';
 export { default as SideBarNav } from '@/components/layout/side-bar/SideBarNav';
+export { default as SideBarFooter } from '@/components/layout/side-bar/SideBarFooter';

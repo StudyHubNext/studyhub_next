87a94fbb56457d6f03575241cdc206c67b3b4bad
minji_105 <ajhjmj9@naver.com>
2025-11-25 00:08:34 +0900

    ✨ Feature: 사이드바 오픈 기능 추가 (#35)

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,20 +1,26 @@
+'use client';
+
 import { Avatar, Logo, Text } from '@/components/common';
 import { Bars3Icon } from '@heroicons/react/20/solid';
+import { useState } from 'react';
 import AuthButtonGroup from '../AuthButtonGroup';
 import UserDropdown from './UserDropdown';
 import HeaderNav from './HeaderNav';
+import SideBar from '../side-bar/SideBar';
 
 interface HeaderProps {
   isLoggedIn: boolean;
 }
 
 export default function Header({ isLoggedIn }: HeaderProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
   return (
     <header className='fixed z-50 h-[var(--header-height)] w-full border-b border-gray-200 bg-white px-4 md:px-20'>
       <div className='m-auto flex h-full max-w-7xl items-center'>
         <div className='flex w-full justify-between'>
           <div className='flex items-center gap-2.5 md:hidden'>
-            <Bars3Icon width='24' className='cursor-pointer' />
+            <Bars3Icon width='24' className='cursor-pointer' onClick={() => setIsMenuOpen(true)} />
             <Logo size='sm' />
           </div>
           <div className='hidden md:flex'>
@@ -36,6 +42,10 @@ export default function Header({ isLoggedIn }: HeaderProps) {
           </div>
         </div>
       </div>
+
+      {isMenuOpen && (
+        <SideBar isLoggedIn={isLoggedIn} isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
+      )}
     </header>
   );
 }

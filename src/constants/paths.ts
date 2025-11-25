f01a82fa24885ecbe3aba792bdde8f3d942d9736
minji_105 <ajhjmj9@naver.com>
2025-11-25 21:29:16 +0900

    ✨ Feature: 마이페이지 경로 세분화 (#66)

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,4 +1,5 @@
 const GROUPS_BASE = '/groups';
+const MYPAGE_BASE = '/mypage';
 const GROUP_DETAIL_BASE = (groupId: string | number) => `${GROUPS_BASE}/${groupId}`;
 const GROUP_RECORDS_BASE = (groupId: string | number) => `${GROUP_DETAIL_BASE(groupId)}/records`;
 const GROUP_RECORD_DETAIL_BASE = (groupId: string | number, recordId: string | number) =>
@@ -8,9 +9,15 @@ export const PATHS = {
   HOME: '/',
   LOGIN: '/login',
   SIGNUP: '/signup',
-  MYPAGE: '/mypage',
   LECTURES:
     'https://www.inflearn.com/?infPartner=true&utm_source=google&utm_medium=pmax&utm_campaign=purchase_regular_g-purchase-main&utm_content=_branding_all&utm_term=_240325_no-2&gad_source=1&gad_campaignid=20714471420',
+
+  MYPAGE: {
+    PROFILE: `${MYPAGE_BASE}/profile`,
+    LECTURES: `${MYPAGE_BASE}/bookmarked-lectures`,
+    APPLICATIONS: `${MYPAGE_BASE}/applications`,
+    STUDY: `${MYPAGE_BASE}/study`,
+  },
 
   GROUPS: {
     LIST: GROUPS_BASE,

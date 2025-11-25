4c9fb7adc229d8fc420794601d7ec2b59be058f1
minji_105 <ajhjmj9@naver.com>
2025-11-26 01:06:25 +0900

    ♻️ Refactor: 로그인 버튼 클릭 시 로그인 액션 호출 (#68)

--- src/components/layout/AuthButtonGroup.tsx
+++ src/components/layout/AuthButtonGroup.tsx
@@ -2,13 +2,15 @@
 
 import { Button } from '@/components/common';
 import { usePageNav } from '@/hooks';
+import { useUserStore } from '@/store/userStore';
 
 export default function AuthButtonGroup() {
-  const { navigateToLogin, navigateToSignup } = usePageNav();
+  const { navigateToSignup } = usePageNav();
+  const { login } = useUserStore();
 
   return (
     <div className='flex gap-2'>
-      <Button variant='ghost' className='flex-grow text-base' onClick={navigateToLogin}>
+      <Button variant='ghost' className='flex-grow text-base' onClick={login}>
         로그인
       </Button>
       <Button className='flex-grow text-base' onClick={navigateToSignup}>

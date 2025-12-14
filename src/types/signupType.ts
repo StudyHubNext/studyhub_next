7f3337c598fd0e3549792fb31d856c295833be9f
Jun <nbc7799@naver.com>
2025-12-14 22:56:42 +0900

    🏷️ Type: 회원가입 ㅋ타입 (#56)

---
+++ src/types/signupType.ts
@@ -0,0 +1,11 @@
+export interface SignupFormData {
+  name: string;
+  nickname: string;
+  birth: string;
+  email: string;
+  emailCode: string;
+  phone: string;
+  phoneCode: string;
+  password: string;
+  passwordConfirm: string;
+}

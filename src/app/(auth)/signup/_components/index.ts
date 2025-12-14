ed6a29decad0f5fe64601ab7c47df861d43ccd72
Jun <nbc7799@naver.com>
2025-12-14 23:35:43 +0900

    ♻️ Refactor: 불필요한 export 코드 제거 (#56)

--- src/app/(auth)/signup/_components/index.ts
+++ src/app/(auth)/signup/_components/index.ts
@@ -1,6 +1,2 @@
-export { default as EmailVerification } from '@/app/(auth)/signup/_components/EmailVerification';
-export { default as FormField } from '@/app/(auth)/signup/_components/FormField';
-export { default as PasswordConfirm } from '@/app/(auth)/signup/_components/PasswordConfirm';
-export { default as PhoneVerification } from '@/app/(auth)/signup/_components/PhoneVerification';
-export { default as SignupForm } from '@/app/(auth)/signup/_components/SignupForm';
-export { default as SignupHeader } from '@/app/(auth)/signup/_components/SignupHeader';
+export { default as SignupForm } from './SignupForm';
+export { default as SignupHeader } from './SignupHeader';

6a6819e135eb37140f78074019f24931bc4ecc50
Jun <nbc7799@naver.com>
2025-12-14 23:09:45 +0900

    ✨ Feature: index파일추가 (#56)

---
+++ src/app/(auth)/signup/_components/index.ts
@@ -0,0 +1,6 @@
+export { default as EmailVerification } from '@/app/(auth)/signup/_components/EmailVerification';
+export { default as FormField } from '@/app/(auth)/signup/_components/FormField';
+export { default as PasswordConfirm } from '@/app/(auth)/signup/_components/PasswordConfirm';
+export { default as PhoneVerification } from '@/app/(auth)/signup/_components/PhoneVerification';
+export { default as SignupForm } from '@/app/(auth)/signup/_components/SignupForm';
+export { default as SignupHeader } from '@/app/(auth)/signup/_components/SignupHeader';

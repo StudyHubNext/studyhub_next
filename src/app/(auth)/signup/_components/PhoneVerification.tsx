7911380e621decb422bd7b1234b9d30710a946ec
Jun <nbc7799@naver.com>
2025-12-14 22:46:46 +0900

    ✨ Feature: 핸드폰번호 입력및 인증 컴포넌트 (#56)

---
+++ src/app/(auth)/signup/_components/PhoneVerification.tsx
@@ -0,0 +1,20 @@
+import FormField from '@/app/(auth)/signup/_components/FormField';
+import { Button } from '@/components';
+import { FORM_LABELS, FORM_PLACEHOLDERS } from '@/constants/signup';
+
+export default function PhoneVerification() {
+  return (
+    <div className='flex flex-col'>
+      <FormField id='phone' label={FORM_LABELS.PHONE} placeholder={FORM_PLACEHOLDERS.PHONE}>
+        <Button variant='primary' className='text-nowrap'>
+          인증코드전송
+        </Button>
+      </FormField>
+      <FormField id='phone' placeholder={FORM_PLACEHOLDERS.PHONE_CODE}>
+        <Button variant='secondary' className='text-nowrap'>
+          인증번호확인
+        </Button>
+      </FormField>
+    </div>
+  );
+}

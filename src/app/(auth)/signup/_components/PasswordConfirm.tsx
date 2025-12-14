284e577a0a062e2cf75159810de44e7659146cef
Jun <nbc7799@naver.com>
2025-12-14 23:18:28 +0900

    ♻️ Refactor: 패스워드컴포넌트 id  수정, 타입 추가 (#56)

--- src/app/(auth)/signup/_components/PasswordConfirm.tsx
+++ src/app/(auth)/signup/_components/PasswordConfirm.tsx
@@ -1,21 +1,20 @@
 import { FORM_LABELS, FORM_PLACEHOLDERS, HELPER_TEXTS } from '@/constants/signup';
 import FormField from '@/app/(auth)/signup/_components/FormField';
-import { Input } from '@/components';
 
 export default function PasswordConfirm() {
   return (
     <div className='flex flex-col gap-2'>
       <FormField
         id='password'
+        type='password'
         label={FORM_LABELS.PASSWORD}
         placeholder={FORM_PLACEHOLDERS.PASSWORD}
         helperText={HELPER_TEXTS.PASSWORD}
       />
-      <Input
-        id='password'
-        type='text'
+      <FormField
+        id='password-confirm'
+        type='password'
         placeholder={FORM_PLACEHOLDERS.PASSWORD_CONFIRM}
-        className='h-12'
       />
     </div>
   );
